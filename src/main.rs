a0d465475ba088bf7f9f7a9489f2748e6f7ef06c
Ole Petersen <peteole2707@gmail.com>
2023-01-13 14:11:34 +0100

    Finalized string handling

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+pub mod datasource;
+pub mod datasources;
+pub mod numeric;
+pub mod search_engine;
+pub mod search_result;
+pub mod string;
+
+#[tokio::main]
+async fn main(){
+    println!("Hello")
+}
--- src/string.rs
+++ src/string.rs
@@ -1,21 +1,20 @@
 use std::collections::HashMap;
 
-
 #[derive(Debug, Clone)]
 pub enum StringFieldValue {
     Exact(String),
     /// Possible values mapped to their probability. If the sum of the probabilities is not 1, the remaining probability is assigned to the "other" value.
     Distribution(HashMap<String, f64>),
-    Error
+    Error,
 }
 
 impl StringFieldValue {
     pub fn merge(v: Vec<Self>) -> Self {
         //panic!("Not implemented");
-        if v.len()==0 {
+        if v.len() == 0 {
             return StringFieldValue::Error;
         }
-        if v.len()==1{
+        if v.len() == 1 {
             return v[0].clone();
         }
         // propagate errors
@@ -25,32 +24,43 @@ impl StringFieldValue {
         }) {
             return StringFieldValue::Error;
         }
-        let mut exacts= v.iter().filter_map(|val| match val {
+        let mut exacts = v.iter().filter_map(|val| match val {
             StringFieldValue::Exact(s) => Some(s),
-            _ => None
+            _ => None,
         });
         if let Some(exact) = exacts.next() {
-            if exacts.clone().all(|s| s==exact) {
+            if exacts.clone().all(|s| s == exact) {
                 return StringFieldValue::Exact(exact.clone());
-            }else {
+            } else {
                 return StringFieldValue::Error;
             }
         }
-        panic!("Not implemented");
         //now only distributions are left
-        let result = HashMap::new();
-        for val in v {
-            match val {
-                StringFieldValue::Exact(s) => {
-                    let count = result.entry(s.clone()).or_insert(0.0);
-                    *count += 1.0;
-                }
-                _ => {}
+        let mut result = HashMap::new();
+        let distributions = v.iter().filter_map(|val| match val {
+            StringFieldValue::Distribution(s) => Some(s),
+            _ => None,
+        });
+        let first_distribution = match distributions.clone().next() {
+            Some(d) => d,
+            None => return StringFieldValue::Error,
+        };
+        let mut probability_sum = 0.0;
+        for (key, _) in first_distribution {
+            let probability = distributions
+                .clone()
+                .map(|d| d.get(key).unwrap_or(&0.0))
+                .product();
+            probability_sum += probability;
+            if probability > 0.0 {
+                result.insert(key.clone(), probability);
             }
         }
-        let total: f64 = result.values().sum();
-        for (_, count) in result.iter_mut() {
-            *count /= total;
+        if probability_sum==0.0{
+            return StringFieldValue::Error;
+        }
+        for (_, value) in result.iter_mut() {
+            *value /= probability_sum;
         }
         StringFieldValue::Distribution(result)
     }
